385f7967bc3544fbe5bd58cec87f966c405943fb
Andrew Raffensperger <raffy@me.com>
2023-02-05 20:52:46 -0800

    update labels

--- dump-count.js
+++ dump-count.js
@@ -1,4 +1,5 @@
 import {read_labels} from './labels.js';
+import {readFileSync, writeFileSync} from 'node:fs';
 
 const LABELS = read_labels();
 
@@ -14,10 +15,29 @@ for (let label of LABELS) {
 let count = new Intl.NumberFormat('en-US').format(LABELS.length);
 let date = new Date().toJSON().split('T')[0];
 
-console.log(`\`${count}\` unique, stop-free labels as of \`${date}\``);
-//console.log(tally);
-console.log(`| N | # | % |`);
-console.log(`| :--- | ---: | ---: |`);
-Object.entries(tally).forEach(([k, v]) => {
-	console.log(`| ${k} | ${v} | ${(100*v/LABELS.length).toFixed(4)}% |`);
-});
+let summary = `\`${count}\` unique, stop-free labels as of \`${date}\``;
+
+let table = [
+	`| N | # | % |`,
+	`| :--- | ---: | ---: |`,
+	...Object.entries(tally).map(([k, v]) => {
+		return `| ${k} | ${v} | ${(100*v/LABELS.length).toFixed(4)}% |`;
+	})
+].join('\n');
+
+console.log(summary);
+
+if (process.argv[2] === 'save') {
+	let file = new URL('./README.md', import.meta.url);
+	let text = readFileSync(file, {encoding: 'utf-8'});
+	let vars = {summary, table};
+	text = text.replace(/<!--\s*([a-z]+)\s*-->.*?<!--\s*\/\1\s*-->/gmsu, (_, k) => {
+		let value = vars[k];
+		if (!value) throw new Error(`expected var: "${k}"`);
+		return `<!-- ${k} -->\n${value}\n<!-- /${k} -->`;
+	});
+	writeFileSync(file, text);
+	console.log(`Updated: ${file}`);
+} else {
+	console.log(table);
+}
